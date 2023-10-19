ddfc1562a1eb2c5f656b47da11c1ce8c6b1c246d
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-10-19 16:37:18 -0300

    Consuming the CitiesContext

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -1,4 +1,4 @@
-import { useState, createContext, useEffect } from "react";
+import { useState, createContext, useEffect, useContext } from "react";
 
 const BASE_URL = "http://localhost:8000";
 
@@ -36,4 +36,11 @@ const CitiesProvider = ({ children }) => {
   );
 };
 
-export { CitiesProvider };
+const useCities = () => {
+  const context = useContext(CitiesContext);
+  if (context === undefined)
+    throw new Error("CitiesContext was used outside the CitiesProvider");
+  return context;
+};
+
+export { CitiesProvider, useCities };
