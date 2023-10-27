ab70407a170b5605a6f9a226b58da2d2d660817b
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-10-27 17:46:37 -0300

    Adding Fake Authentication: Implementing Login

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -19,7 +19,7 @@ const reducer = (state, action) => {
 };
 
 const FAKE_USER = {
-  name: "Jack",
+  name: "Marshal",
   email: "jack@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
@@ -51,6 +51,7 @@ const useAuth = () => {
   const context = useContext(AuthContext);
   if (context === undefined)
     throw new Error("AuthContext was used outside AuthProvider");
+  return context;
 };
 
 export { AuthProvider, useAuth };
